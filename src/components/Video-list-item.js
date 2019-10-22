b3a780f241f6acbfe461e6b622c9e28e34a0b6da
Pierre <pm4rtin@gmail.com>
2019-10-22 13:43:19 +0200

    added css with bootstrap

--- src/components/Video-list-item.js
+++ src/components/Video-list-item.js
@@ -1,9 +1,16 @@
 import React from 'react';
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 const VideoListItem = ({movie}) => {
-    return <li>
-        <img height="150px" src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={`Film ${movie.title}`}/>
-        <h3>{movie.title}</h3>
+    return <li className="list-group-item">
+        <div className="media">
+            <div className="media-left">
+                 <img className="media-object img-rounded" height="150px" src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={`Film ${movie.title}`}/>
+            </div>
+        
+            <div className="media-body">
+                <h5 className="title_list_item">{movie.title}</h5>
+            </div>
+        </div>
         </li>
 }
 
